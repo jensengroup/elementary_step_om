b393c6eeb07fd91e7d00b8b65eabdd7495bdcae6
Mads Koerstz <madsen.koerstz@gmail.com>
2020-10-20 12:15:57 +0200

    Add code to generate one-step products
    This code does not work for organometals.

---
+++ elementary_step_om/elementary_step.py
@@ -0,0 +1,165 @@
+from itertools import combinations as comb
+from itertools import product as prod
+import copy
+import numpy as np
+
+
+from rdkit import Chem
+from rdkit.Chem import rdMolDescriptors, rdchem
+from rdkit.Chem.rdchem import ResonanceFlags
+from rdkit.Chem.EnumerateStereoisomers import EnumerateStereoisomers
+from rdkit.Chem.EnumerateStereoisomers import StereoEnumerationOptions
+from rdkit.Chem import rdmolops
+
+
+from .xyz2mol_local import AC2mol, get_proto_mol
+
+
+max_valence = {}
+
+max_valence[1] = 1
+max_valence[6] = 4
+max_valence[7] = 4
+max_valence[8] = 3
+max_valence[9] = 1
+max_valence[14] = 4
+max_valence[15] = 5
+max_valence[16] = 6
+max_valence[17] = 1
+max_valence[35] = 1
+max_valence[53] = 1
+
+
+def valid_product(product_adj_matrix, atomic_num):
+    """ Check that the produces product is valid according to the valence"""
+    product_valence = product_adj_matrix.sum(axis=1, dtype=np.int16)
+    for atom, valence in zip(atomic_num, product_valence):
+        if valence > max_valence[atom]:
+            return False
+    return True
+
+
+def get_most_rigid_resonance(mol):
+    """ Return the most rigid resonance structure """
+    all_resonance_structures = [res for res in rdchem.ResonanceMolSupplier(mol,
+                                ResonanceFlags.UNCONSTRAINED_ANIONS)]
+
+    if len(all_resonance_structures) == 1:
+        return all_resonance_structures[0]
+    else:
+        min_rot_bonds = 9999
+        for res in all_resonance_structures:
+            Chem.SanitizeMol(res)
+            num_rot_bonds = rdMolDescriptors.CalcNumRotatableBonds(res)
+            if num_rot_bonds < min_rot_bonds:
+                most_rigid_res = copy.deepcopy(res)
+                min_rot_bonds = num_rot_bonds
+
+        return most_rigid_res
+
+
+def reassign_atom_idx(mol):
+    """ Assigns RDKit mol atom id to atom mapped id """
+    renumber = [(atom.GetIdx(), atom.GetAtomMapNum()) for atom in mol.GetAtoms()]
+    new_idx = [idx[0] for idx in sorted(renumber, key=lambda x: x[1])]
+
+    return Chem.RenumberAtoms(mol, new_idx)
+
+
+def set_chirality(product, reactant):
+    """ Produce all combinations of isomers (R/S and cis/trans). But force 
+    product atoms with unchanged neighbors to the same label chirality as
+    the reactant """
+
+    # TODO move these somewhere it makes more sense.
+    product = reassign_atom_idx(product)
+    reactant = reassign_atom_idx(reactant)
+
+    Chem.SanitizeMol(product)
+    Chem.SanitizeMol(reactant)
+
+    # Find chiral atoms - including label chirality
+    chiral_atoms_product = Chem.FindMolChiralCenters(product, includeUnassigned=True)
+
+    unchanged_atoms = []
+    for atom, chiral_tag in chiral_atoms_product:
+        product_neighbors = [a.GetIdx() for a in product.GetAtomWithIdx(atom).GetNeighbors()]
+        reactant_neighbors = [a.GetIdx() for a in reactant.GetAtomWithIdx(atom).GetNeighbors()]
+        
+        if sorted(product_neighbors) == sorted(reactant_neighbors):
+            unchanged_atoms.append(atom)
+
+    # make combinations of isomers.
+    opts = StereoEnumerationOptions(onlyUnassigned=False, unique=False)
+    rdmolops.AssignStereochemistry(product, cleanIt=True,
+                                   flagPossibleStereoCenters=True, force=True)
+
+    product_isomers = []
+    product_isomers_mols = []
+    for product_isomer in EnumerateStereoisomers(product, options=opts):
+        rdmolops.AssignStereochemistry(product_isomer, force=True)
+        for atom in unchanged_atoms:
+            reactant_global_tag = reactant.GetAtomWithIdx(atom).GetProp('_CIPCode')
+
+            # TODO make sure that the _CIPRank is the same for atom in reactant and product.
+            product_isomer_global_tag = product_isomer.GetAtomWithIdx(atom).GetProp('_CIPCode')
+            if reactant_global_tag != product_isomer_global_tag:
+                product_isomer.GetAtomWithIdx(atom).InvertChirality()
+
+        if Chem.MolToSmiles(product_isomer) not in product_isomers:
+            product_isomers.append(Chem.MolToSmiles(product_isomer))
+            product_isomers_mols.append(product_isomer)
+
+    return product_isomers_mols
+
+
+def valid_products(reactant, n=2, cd=4):
+    """ General generator that produces hypothetical valid 
+    one-step products from reactant. 
+    
+    * n (int) - number of bonds to break/form
+    * cd (int) - max value for #break + #form <= cd 
+    """
+    reactant_adj_matrix = Chem.GetAdjacencyMatrix(reactant)
+    atomic_num = [atom.GetAtomicNum() for atom in reactant.GetAtoms()]
+
+    # Create the one bond conversion matrices
+    adjacency_matrix_shape = reactant_adj_matrix.shape
+    make1, break1 = [], []
+    for i in range(adjacency_matrix_shape[0]):
+        for j in range(adjacency_matrix_shape[1]):
+            conversion_matrix = np.zeros(adjacency_matrix_shape, np.int8)
+            if j > i:
+                if reactant_adj_matrix[i, j] == 0:
+                    conversion_matrix[i, j] = conversion_matrix[j, i] = 1
+                    make1.append(conversion_matrix)
+                else:
+                    conversion_matrix[i, j] = conversion_matrix[j, i] = -1
+                    break1.append(conversion_matrix)
+
+    # Use one bond conversion matrices to create the remaining
+    # conversion matrices
+    comb_to_check = [c for c in prod(range(n+1), repeat=2)
+                     if sum(c) <= cd and max(c) <= n]
+
+    for num_make, num_brake in comb_to_check[1:]:  # skip (0, 0)
+        for x in prod(comb(make1, num_make), comb(break1, num_brake)):
+            conversion_matrix = np.array(sum(x, ())).sum(axis=0)
+            product_adj_matrix = reactant_adj_matrix + conversion_matrix
+            if valid_product(product_adj_matrix, atomic_num):
+                proto_mol = get_proto_mol(atomic_num)
+                mol = AC2mol(proto_mol, product_adj_matrix, atomic_num, 0,
+                             allow_charged_fragments=True, use_graph=True,
+                             use_atom_maps=True)
+                Chem.Kekulize(mol, clearAromaticFlags=True)
+
+                # Hack is this OK?
+                if int(Chem.GetFormalCharge(mol)) != 0:
+                    continue
+
+                # Get most rigid mol.
+                mol = get_most_rigid_resonance(mol)
+
+                # Get stereo isomers:
+                for isomer in set_chirality(mol, reactant):
+                    yield isomer
